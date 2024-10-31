1ec30a3fbe5e6bc8e0322482f2c96c2159b0736b
JokingOff <jtoscano1530@gmail.com>
2024-10-31 16:56:34 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+// Import Firebase app and database from firebaseConfig.js
+import { db } from "./firebaseConfig.js";
 
 // Screen elements
 const screens = {
